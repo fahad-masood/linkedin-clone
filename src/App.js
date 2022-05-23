345a309fb31731ec0dc3cc6ce22d279d0d703a50
Fahad <fahadmasood03@gmail.com>
2022-05-23 11:53:23 +0530

    integrate redux

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 
 function App() {
+  // Pull the user from userSlice
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       {/* Header */}
       <Header />
+
       {/* App body */}
       <div className="app_body">
         {/* Sidebar */}
